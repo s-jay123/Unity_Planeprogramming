b714ab120581c9149a00cb06db993be96ffcd360
sjay <saira.jamil64@gmail.com>
2021-01-24 20:56:18 +0100

    First commit

---
+++ Assets/Challenge 1/Scripts/RotatePropeller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePropeller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float rotationSpeed= 300.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        //rotation on Z axis,
+        transform.Rotate(0,0, rotationSpeed * Time.deltaTime);
+
+
+
+    }
+}
